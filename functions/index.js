b03a08c1e75d3d51a1a681bb87b53d9c8be1d1f5
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-02 15:56:39 -0400

    added delete route and function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import functions from "firebase-functions"
 import express from "express"
 import cors from "cors"
-import { createNewAlbum, getAllAlbums } from "./src/albums.js"
+import { createNewAlbum, deleteOneAlbum, getAllAlbums } from "./src/albums.js"
 
 const app = express()
 app.use(cors())
@@ -9,5 +9,6 @@ app.use(express.json())
 
 app.get("/albums", getAllAlbums)
 app.post("/albums", createNewAlbum)
+app.delete("/albums/:albumId", deleteOneAlbum)
 
 export const api = functions.https.onRequest(app)
--- functions/src/albums.js
+++ functions/src/albums.js
@@ -19,3 +19,11 @@ export function getAllAlbums(req, res) {
     .catch(err => res.status(500).send({success: false, message: err}))
   };
   
+
+  export function deleteOneAlbum(req, res) {
+    const db = dbConnect()
+    const { albumId } = req.params
+    db.collection('albums').doc(albumId).delete()
+      .then(res.status(201).send({message: 'Album deleted'}))
+      .catch(err => res.status(500).send({success: false, message: err}))
+  }
